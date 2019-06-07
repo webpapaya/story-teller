82b5d1871ba5d54b423d347b3cc17e710a12730c
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-06-07 23:20:15 +0200

    Added very basic event sourcing implementation.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,7 @@
-it('test', () => {
-  console.log('hallo');
+import { createApp } from ".";
+
+it('create user', async () => {
+  const app = createApp();
+  await app.publish({ type: 'user/created', payload: { id: 1, name: 'Test'} });
+  console.log(app.read());
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-console.log('hell')
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+type EventId = number;
+export type SingleEvent<Type, Payload> = {
+  type: Type,
+  payload: Payload,
+}
+
+export type User = {
+  id: number,
+  name: string
+}
+
+type AllEvents =
+  | SingleEvent<'user/created', User>
+  | SingleEvent<'user/updated', User>
+  | SingleEvent<'user/deleted', Pick<User, 'id'>>
+
+type InternalEvent = { eventId: EventId } & AllEvents;
+
+type Events = AllEvents[]
+type State = {
+  users: User[]
+}
+
+const reducer = async (event:InternalEvent, state:State): Promise<void> => {
+  switch (event.type) {
+    case 'user/created':
+      state.users.push(event.payload);
+    case 'user/updated':
+      const updateUser = state.users.find((user) => user.id === event.payload.id);
+      if (updateUser) {
+        updateUser.name = event.payload.name
+      }
+    case 'user/deleted':
+      const nextUsers = state.users.filter((user) => user.id === event.payload.id);
+      state.users = nextUsers;
+  }
+
+  return Promise.resolve();
+}
+
+export const createApp = () => {
+  const state: State = { users: [] }
+  const store: Events = [];
+  const publish = async (event:AllEvents): Promise<EventId> => {
+    const eventId = +new Date() + Math.random();
+    const internalEvent = { ...event, eventId }
+    store.push(internalEvent);
+    await reducer(internalEvent, state);
+    return eventId;
+  }
+
+  const read = () => state;
+
+  return { publish, read };
+}
