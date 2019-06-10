33a7fe6ad89489a5763f9715efc0d17c2cb96ce7
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-06-10 11:03:01 +0200

    Scope user tests.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,35 +1,37 @@
+import expect from "expect";
 import { createApp } from ".";
 import { t } from "./db";
-import expect from "expect";
 
-it('create user', t(async (withinConnection) => {
-  const app = createApp(withinConnection);
-  await app.publish({ type: 'user/created', payload: { id: 1, name: 'Test'} });
-  await withinConnection(async ({ client }) => {
-    const result = await client.query(`select * from Users where id = 1;`);
-    expect(result.rows[0].name).toEqual('Test');
-  });
-}));
+describe('user', () => {
+  it('create', t(async (withinConnection) => {
+    const app = createApp(withinConnection);
+    await app.publish({ type: 'user/created', payload: { id: 1, name: 'Test'} });
+    await withinConnection(async ({ client }) => {
+      const result = await client.query(`select * from Users where id = 1;`);
+      expect(result.rows[0].name).toEqual('Test');
+    });
+  }));
 
-it('update user', t(async (withinConnection) => {
-  const app = createApp(withinConnection);
-  await app.publish({ type: 'user/created', payload: { id: 1, name: 'Test'} });
-  await app.publish({ type: 'user/updated', payload: { id: 1, name: 'Updated'} });
-  await withinConnection(async ({ client }) => {
-    const result = await client.query(`select * from Users where id = 1;`);
-    expect(result.rows[0].name).toEqual('Updated');
-  });
-}));
+  it('update', t(async (withinConnection) => {
+    const app = createApp(withinConnection);
+    await app.publish({ type: 'user/created', payload: { id: 1, name: 'Test'} });
+    await app.publish({ type: 'user/updated', payload: { id: 1, name: 'Updated'} });
+    await withinConnection(async ({ client }) => {
+      const result = await client.query(`select * from Users where id = 1;`);
+      expect(result.rows[0].name).toEqual('Updated');
+    });
+  }));
 
-it('delete user', t(async (withinConnection) => {
-  const app = createApp(withinConnection);
-  await app.publish({ type: 'user/created', payload: { id: 1, name: 'Test'} });
-  await app.publish({ type: 'user/deleted', payload: { id: 1} });
-  await withinConnection(async ({ client }) => {
-    const result = await client.query(`select * from Users where id = 1;`);
-    expect(result.rows[0]).toEqual(undefined);
-  });
-}));
+  it('delete', t(async (withinConnection) => {
+    const app = createApp(withinConnection);
+    await app.publish({ type: 'user/created', payload: { id: 1, name: 'Test'} });
+    await app.publish({ type: 'user/deleted', payload: { id: 1} });
+    await withinConnection(async ({ client }) => {
+      const result = await client.query(`select * from Users where id = 1;`);
+      expect(result.rows[0]).toEqual(undefined);
+    });
+  }));
+});
 
 it('rebuild aggregates', t(async (withinConnection) => {
   const app = createApp(withinConnection);
