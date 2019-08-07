c3bd0eb5288a168d660ba138fc3a3867cc60de01
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-08-08 00:08:31 +0200

    Restructured tests.

--- src/stamps-to-bookings/index.spec.ts
+++ src/stamps-to-bookings/index.spec.ts
@@ -164,187 +164,189 @@ describe('stampsToBooking', () => {
     })
   })
 
-  describe('correlationDate threshold', () => {
-    it('all stamps are on same day (date of first stamp)', () => {
-      const stamps = [
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-01T00:00:00Z'),
-          type: 'Start'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-01T01:00:00Z'),
-          type: 'Stop'
-        })
-      ]
+  describe('correlationDate', () => {
+    describe('threshold', () => {
+      it('all stamps are on same day (date of first stamp)', () => {
+        const stamps = [
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-01T00:00:00Z'),
+            type: 'Start'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-01T01:00:00Z'),
+            type: 'Stop'
+          })
+        ]
 
-      assertThat(stampsToBookings(configFactory.build(), stamps), hasProperties({
-        0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
-      }))
-    })
-
-    it('stamps pass dateline (date of first stamp)', () => {
-      const stamps = [
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
-          type: 'Start'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
-          type: 'Start'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T02:00:00Z'),
-          type: 'Stop'
-        })
-      ]
-
-      assertThat(stampsToBookings(configFactory.build(), stamps), hasProperties({
-        0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate()),
-        1: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
-      }))
-    })
-
-    it('with small gap below threshold (date of first stamp)', () => {
-      const stamps = [
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
-          type: 'Start'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
-          type: 'Stop'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T01:00:05Z'),
-          type: 'Start'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T02:00:00Z'),
-          type: 'Stop'
-        })
-
-      ]
-
-      assertThat(stampsToBookings(configFactory.build({
-        correlationDate: { threshold: Duration.ofMinutes(1) }
-      }), stamps), hasProperties({
-        0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate()),
-        1: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
-      }))
-    })
-
-    it('with small gap exactly on threshold (date of first stamp)', () => {
-      const stamps = [
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
-          type: 'Start'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
-          type: 'Stop'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T01:01:00Z'),
-          type: 'Start'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T02:00:00Z'),
-          type: 'Stop'
-        })
-      ]
-
-      assertThat(stampsToBookings(configFactory.build({
-        correlationDate: { threshold: Duration.ofMinutes(1) }
-      }), stamps), hasProperties({
-        0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate()),
-        1: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
-      }))
-    })
-
-    it('when big gap on next day (date of start stamp)', () => {
-      const stamps = [
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
-          type: 'Start'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
-          type: 'Stop'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T02:00:00Z'),
-          type: 'Start'
-        }),
-        stampFactory.build({
-          timestamp: ZonedDateTime.parse('2000-01-02T03:00:00Z'),
-          type: 'Stop'
-        })
-      ]
-
-      assertThat(stampsToBookings(configFactory.build({
-        correlationDate: { threshold: Duration.ofMinutes(1) }
-      }), stamps), hasProperties({
-        0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate()),
-        1: hasProperty('correlationDate', stamps[2].timestamp.toLocalDate())
-      }))
-    })
-  })
-
-  describe('correlationDatePosition:', () => {
-    const stamps = [
-      stampFactory.build({
-        timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
-        type: 'Start'
-      }),
-      stampFactory.build({
-        timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
-        type: 'Stop'
+        assertThat(stampsToBookings(configFactory.build(), stamps), hasProperties({
+          0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
+        }))
       })
-    ]
 
-    it('start', () => {
-      assertThat(stampsToBookings(configFactory.build({
-        correlationDate: { position: 'start' }
-      }), stamps), hasProperties({
-        0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
-      }))
-    })
+      it('stamps pass dateline (date of first stamp)', () => {
+        const stamps = [
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
+            type: 'Start'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
+            type: 'Start'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T02:00:00Z'),
+            type: 'Stop'
+          })
+        ]
 
-    it('middle', () => {
-      assertThat(stampsToBookings(configFactory.build({
-        correlationDate: { position: 'center' }
-      }), stamps), hasProperties({
-        0: hasProperty('correlationDate', stamps[1].timestamp.toLocalDate())
-      }))
-    })
-
-    it('end', () => {
-      assertThat(stampsToBookings(configFactory.build({
-        correlationDate: { position: 'end' }
-      }), stamps), hasProperties({
-        0: hasProperty('correlationDate', stamps[1].timestamp.toLocalDate())
-      }))
-    })
-  })
-
-  describe('correlationDate: startOfDay', () => {
-    const stamps = [
-      stampFactory.build({
-        timestamp: ZonedDateTime.parse('2000-01-02T00:00:00Z'),
-        type: 'Start'
-      }),
-      stampFactory.build({
-        timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
-        type: 'Stop'
+        assertThat(stampsToBookings(configFactory.build(), stamps), hasProperties({
+          0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate()),
+          1: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
+        }))
       })
-    ]
 
-    it('when day starts at noon, times will be shifted to previous day', () => {
-      assertThat(stampsToBookings(configFactory.build({
-        correlationDate: { startOfDay: LocalTime.NOON }
-      }), stamps), hasProperties({
-        0: hasProperty('correlationDate', LocalDate.parse('2000-01-01'))
-      }))
+      it('with small gap below threshold (date of first stamp)', () => {
+        const stamps = [
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
+            type: 'Start'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
+            type: 'Stop'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T01:00:05Z'),
+            type: 'Start'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T02:00:00Z'),
+            type: 'Stop'
+          })
+
+        ]
+
+        assertThat(stampsToBookings(configFactory.build({
+          correlationDate: { threshold: Duration.ofMinutes(1) }
+        }), stamps), hasProperties({
+          0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate()),
+          1: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
+        }))
+      })
+
+      it('with small gap exactly on threshold (date of first stamp)', () => {
+        const stamps = [
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
+            type: 'Start'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
+            type: 'Stop'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T01:01:00Z'),
+            type: 'Start'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T02:00:00Z'),
+            type: 'Stop'
+          })
+        ]
+
+        assertThat(stampsToBookings(configFactory.build({
+          correlationDate: { threshold: Duration.ofMinutes(1) }
+        }), stamps), hasProperties({
+          0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate()),
+          1: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
+        }))
+      })
+
+      it('when big gap on next day (date of start stamp)', () => {
+        const stamps = [
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
+            type: 'Start'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
+            type: 'Stop'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T02:00:00Z'),
+            type: 'Start'
+          }),
+          stampFactory.build({
+            timestamp: ZonedDateTime.parse('2000-01-02T03:00:00Z'),
+            type: 'Stop'
+          })
+        ]
+
+        assertThat(stampsToBookings(configFactory.build({
+          correlationDate: { threshold: Duration.ofMinutes(1) }
+        }), stamps), hasProperties({
+          0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate()),
+          1: hasProperty('correlationDate', stamps[2].timestamp.toLocalDate())
+        }))
+      })
+    })
+
+    describe('position', () => {
+      const stamps = [
+        stampFactory.build({
+          timestamp: ZonedDateTime.parse('2000-01-01T23:00:00Z'),
+          type: 'Start'
+        }),
+        stampFactory.build({
+          timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
+          type: 'Stop'
+        })
+      ]
+
+      it('start', () => {
+        assertThat(stampsToBookings(configFactory.build({
+          correlationDate: { position: 'start' }
+        }), stamps), hasProperties({
+          0: hasProperty('correlationDate', stamps[0].timestamp.toLocalDate())
+        }))
+      })
+
+      it('middle', () => {
+        assertThat(stampsToBookings(configFactory.build({
+          correlationDate: { position: 'center' }
+        }), stamps), hasProperties({
+          0: hasProperty('correlationDate', stamps[1].timestamp.toLocalDate())
+        }))
+      })
+
+      it('end', () => {
+        assertThat(stampsToBookings(configFactory.build({
+          correlationDate: { position: 'end' }
+        }), stamps), hasProperties({
+          0: hasProperty('correlationDate', stamps[1].timestamp.toLocalDate())
+        }))
+      })
+    })
+
+    describe('startOfDay', () => {
+      const stamps = [
+        stampFactory.build({
+          timestamp: ZonedDateTime.parse('2000-01-02T00:00:00Z'),
+          type: 'Start'
+        }),
+        stampFactory.build({
+          timestamp: ZonedDateTime.parse('2000-01-02T01:00:00Z'),
+          type: 'Stop'
+        })
+      ]
+
+      it('when day starts at noon, times will be shifted to previous day', () => {
+        assertThat(stampsToBookings(configFactory.build({
+          correlationDate: { startOfDay: LocalTime.NOON }
+        }), stamps), hasProperties({
+          0: hasProperty('correlationDate', LocalDate.parse('2000-01-01'))
+        }))
+      })
     })
   })
 })
