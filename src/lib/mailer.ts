541725e088f718dec0317a25f73dfa24bfddf09e
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-08-17 17:47:19 +0200

    Added email sending.

---
+++ src/lib/mailer.ts
@@ -0,0 +1,26 @@
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  pool: true,
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT || ''),
+  auth: {
+    user: process.env.SMTP_HOST_USERNAME,
+    pass: process.env.SMTP_HOST_PASSWORD
+  }
+})
+
+type SendMail = (options: {
+  to: string
+  subject: string
+  html: string
+}) => Promise<void>
+
+export const sendMail: SendMail = async ({ to, subject, html }) => {
+  return transporter.sendMail({
+    from: process.env.SMTP_FROM,
+    to,
+    subject,
+    html
+  })
+}
