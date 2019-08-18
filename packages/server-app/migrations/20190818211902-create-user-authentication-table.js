b6e07f1ece1820935f28d8397b4c2e59b9364e7c
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-08-18 23:25:16 +0200

    Added user-authentication migration.

---
+++ packages/server-app/migrations/20190818211902-create-user-authentication-table.js
@@ -0,0 +1,22 @@
+'use strict'
+
+exports.up = function (db) {
+  return db.runSql(`
+    create table if not exists
+    public.user_authentication (
+      id                uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
+      username          text,
+      password          text
+    );
+  `)
+}
+
+exports.down = function (db) {
+  return db.runSql(`
+    drop table if exists public.user_authentication
+  `)
+}
+
+exports._meta = {
+  'version': 1
+}
--- packages/server-app/src/index.ts
+++ packages/server-app/src/index.ts
@@ -2,8 +2,47 @@ import express from 'express'
 const app = express()
 const port = process.env.API_PORT
 
+
+import passport from 'passport'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import bodyParser from 'body-parser'
+
+app.use(cookieParser());
+app.use(bodyParser());
+// @ts-ignore
+app.use(session({ secret: process.env.SECRET_KEY_BASE }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+import LocalStrategy from 'passport-local';
+passport.use(new LocalStrategy.Strategy(
+  (username, password, done) => {
+    const user = { username, password }
+    return done(null, user);
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  console.log(user)
+  done(null, user);
+});
+
+const userAuth = passport.authenticate('local', { session: true })
+
+app.post('/login', userAuth, (req: express.Request, res: express.Response) => {
+  console.log(req.body)
+  res.send('Hello World!!')
+})
 app.get('/', (req: express.Request, res: express.Response) => {
+  console.log(req.body)
   res.send('Hello World!!')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
