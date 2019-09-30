57592a1cce703dc75aa9dc3f79f5ef90dcce6384
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-09-30 20:59:27 +0200

    Added noMargin option to headings.

--- packages/browser-app/src/components/heading.tsx
+++ packages/browser-app/src/components/heading.tsx
@@ -5,16 +5,18 @@ import { css } from '../utils/css';
 type HeadingProps = {
   tagName?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
   variant?: 'h1' | 'h2' | 'h3' | 'h4'
+  noMargin?: boolean,
   children: React.ReactNode,
 }
 
 const Heading = ({
   children,
   tagName = 'h1',
-  variant = 'h1'
+  variant = 'h1',
+  noMargin = false
 }: HeadingProps) => (
   React.createElement(tagName, {
-    className: css(styles.default, styles[variant] )
+    className: css(styles.default, styles[variant], noMargin && styles.noMargin)
   }, children)
 )
 
