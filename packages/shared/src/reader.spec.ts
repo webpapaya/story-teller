be77ca0134823c7fcd8e0f2143ea3367d709610f
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-12-19 23:22:53 +0100

    Add reader monad

---
+++ packages/shared/src/reader.spec.ts
@@ -0,0 +1,22 @@
+
+class ReaderMonad<A, B> {
+  constructor(public runReader: (a: A) => Promise<B>) {
+  }
+
+  bind<C>(func: (b: B) => ReaderMonad<A, C> | C): ReaderMonad<A, C> {
+    return new ReaderMonad<A, C>((a: A) => {
+      return this.runReader(a)
+        .then((b) => {
+          const rtn = func(b)
+          if (rtn instanceof ReaderMonad) {
+            return rtn.runReader(a)
+          }
+          return ReaderMonad.return(rtn).runReader(a)
+        });
+    });
+  }
+
+  static return<A, B>(b: B): ReaderMonad<A, B> {
+    return new ReaderMonad((a: A) => Promise.resolve(b));
+  }
+}
