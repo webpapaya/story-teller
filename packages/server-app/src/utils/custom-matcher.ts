80d59dc56a565889336ba737bdc4a275ba1472a1
Thomas Mayrhofer <thomas@mayrhofer.at>
2020-07-29 20:44:01 +0200

    add domain event matcher

--- packages/server-app/src/utils/custom-matcher.ts
+++ packages/server-app/src/utils/custom-matcher.ts
@@ -1,4 +1,5 @@
-import { hasProperty, Matcher } from 'hamjest'
+import { hasProperty, Matcher, truthy } from 'hamjest'
 
 export const hasAggregate = (matcher: Matcher) => hasProperty('0', matcher)
 export const hasEvents = (matcher: Matcher) => hasProperty('1', matcher)
+export const present = truthy
---
+++ packages/server-app/src/utils/events.ts
@@ -0,0 +1,16 @@
+import { AnyCodec, v } from "@story-teller/shared"
+
+export const domainEvent = <Name extends string, Codec extends AnyCodec>(name: Name, codec: Codec) => {
+  return {
+    event: v.record({
+      name: v.literal(name),
+      payload: v.record({
+        aggregate: codec
+      })
+    }),
+    mapper: ({ aggregateAfter }: { aggregateAfter: Codec['O'] }) => ({
+      name: name,
+      payload: { aggregate: aggregateAfter }
+    })
+  }
+}
