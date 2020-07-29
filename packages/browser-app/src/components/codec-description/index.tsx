6ca2c002e14435a892114713c76edb2fdc146ab6
Thomas Mayrhofer <thomas@mayrhofer.at>
2020-07-29 18:50:00 +0200

    add code descriptions block

--- packages/browser-app/src/components/codec-description/index.tsx
+++ packages/browser-app/src/components/codec-description/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+
 import { AnyCodec } from '@story-teller/shared';
 import Heading from '../heading';
 import Paragraph from '../paragraph';
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+// @ts-ignore
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const CodecDescription = (props: { name: string, description: string }) => {
   return (
     <div>
       <Heading>{props.name}</Heading>
       <Paragraph>{props.description}</Paragraph>
+      <SyntaxHighlighter language="javascript" style={docco}>
+        test
+
+      </SyntaxHighlighter>
     </div>
   )
 }
