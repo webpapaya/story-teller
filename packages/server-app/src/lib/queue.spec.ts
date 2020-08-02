54c2c1d67be56d24528f86a4938dc90368e7033c
Thomas Mayrhofer <thomas@mayrhofer.at>
2020-08-02 20:52:45 +0200

    remove pg-boss and replace with rabbitmq

--- packages/server-app/src/lib/queue.spec.ts
+++ packages/server-app/src/lib/queue.spec.ts
@@ -1,5 +1,6 @@
 import { t } from '../spec-helpers';
 import { publish, subscribe } from './queue';
+import { assertThat, equalTo } from 'hamjest';
 
 const buildLazyPromise = () => {
   let resolve: () => void;
@@ -9,17 +10,28 @@ const buildLazyPromise = () => {
 
 describe('queue', () => {
   const QUEUE = 'test'
+  const MESSAGE = { hallo: 1 } as const
   it('WHEN published after subscription, subscribes', t(async ({ channel }) => {
     const { resolve, promise } = buildLazyPromise()
-    publish(QUEUE, { hallo: 1 }, channel)
+    publish(QUEUE, MESSAGE, channel)
     subscribe(QUEUE, resolve, channel)
     await promise
   }))
 
   it('WHEN published before subscription, subscribes', t(async ({ channel }) => {
     const { resolve, promise } = buildLazyPromise()
-    publish(QUEUE, { hallo: 1 }, channel)
+    publish(QUEUE, MESSAGE, channel)
     subscribe(QUEUE, resolve, channel)
+    await promise
+  }))
+
+  it('automatically parses JSON input', t(async ({ channel }) => {
+    const { resolve, promise } = buildLazyPromise()
+    publish(QUEUE, MESSAGE, channel)
+    subscribe(QUEUE, (response) => {
+      assertThat(response, equalTo(MESSAGE))
+      resolve()
+    }, channel)
     await promise
   }))
 })
